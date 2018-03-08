5a6bc398ff13e5159a9c6f6fb6c9c6484087a102
varunprakas <varun.prakas359@gmail.com>
2018-03-08 20:46:17 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  name:string;
+
+  constructor(){
+  	this.name = "test"
+  }
+  reset(){
+  	this.name = "test";
+  }
+}
